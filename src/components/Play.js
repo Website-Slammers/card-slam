90ef58a568814edc5cc7f4a3b7a954090a5c2ea9
Andrew Cook <andrewjcook93@gmail.com>
2023-03-02 17:41:42 -0600

    add poke picker to play page

--- src/components/Play.js
+++ src/components/Play.js
@@ -14,9 +14,11 @@ function Play() {
 
       <div className="play__main">
 
-        <Link className="play__card play__card--hearts" to="/play/Hearts"><span>Hearts</span></Link>
+        <Link className="play__card play__card--hearts" to="/play/Hearts"><span>
+          Hearts</span></Link>
 
-        <Link className="play__card play__card--spades" to="/play/Spades"><span>Spades</span></Link>
+        <Link className="play__card play__card--spades" to="/play/Spades"><span>
+          Spades</span></Link>
 
       </div>
 
@@ -24,13 +26,20 @@ function Play() {
 
       <div className="play__main">
 
-        <a className="play__card play__card--sudoku" target="_blank" href="https://sudoku-slam.netlify.app"><span>Sudoku</span></a>
+        <a className="play__card play__card--sudoku" target="_blank" href="https://sudoku-slam.netlify.app"><span>
+          Sudoku</span></a>
 
-        <a className="play__card play__card--connect-four" target="_blank" href="https://connect4bydrewford.netlify.app"><span>Connect 4</span></a>
+        <a className="play__card play__card--connect-four" target="_blank" href="https://connect4bydrewford.netlify.app"><span>
+          Connect 4</span></a>
 
-        <a className="play__card play__card--pixelate" target="_blank" href="https://pixel8game.netlify.app"><span>Pixelate</span></a>
+        <a className="play__card play__card--pixelate" target="_blank" href="https://pixel8game.netlify.app"><span>
+          Pixelate</span></a>
 
-        <a className="play__card play__card--puppy-clicker" target="_blank" href="https://puppy-clicker.netlify.app"><span>Puppy Clicker</span></a>
+        <a className="play__card play__card--puppy-clicker" target="_blank" href="https://puppy-clicker.netlify.app"><span>
+          Puppy Clicker</span></a>
+
+        <a className="play__card play__card--pokemon-picker" target="_blank" href="https://gotta-pick-em-all.netlify.app"><span>
+          Pok&eacute; Picker</span></a>
 
 
       </div>
