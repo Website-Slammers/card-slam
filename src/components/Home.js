dd9a04b72be07dd1418a4e317b065a68978e8a9a
Andrew Cook <andrewjcook93@gmail.com>
2023-03-04 13:14:20 -0600

    grid on home-header and home-featured

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import HeaderMain from './HeaderMain'
-import Featured from './Featured'
 import Play from './Play'
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       
-      <HeaderMain />
-      <Featured />
-      {/* <Play /> */}
+      {/* Home Header Start */}
+      <div className="header-main">
+        <div className="header-main__textbox">
+          <h1 className="heading-primary heading-primary--header-main">
+              <span className="heading-primary--main">
+                CARD GAMES</span>
+              {/* <span className="heading-primary--sub">
+                by Web Slammers</span> */}
+          </h1>
+          <br />
+          <Link to="/play" className="btn btn--header-main">
+              Play<span></span></Link>
+        </div>
+        {/* Header PNG */}
+        <div className="header-main__img">
+          <img src="https://i.imgur.com/t3TQZmq.png" />
+        </div>
+      </div> {/* Home Header End */}
+
+      {/* Featured Games Start */}
+      <div className="featured">
+
+        <h2 className="featured__title">Featured</h2>
+
+        <Link className="featured--lg" to="/play/hearts">
+          <div className="featured__feature featured__feature--hearts">
+            <p>Hearts</p></div></Link>
+
+        <Link className="featured--sm-1"to="/play/spades"><div className="featured__feature featured__feature--spades">
+          <p>Spades</p></div></Link>
+
+        <Link className="featured--sm-2" to="/play/blackjack"><div className="featured__feature featured__feature--blackjack">
+          <p>Blackjack</p></div></Link>
+
+      </div>
+      {/* Featured Games End */}
+
+
 
     </div>
   )
---
+++ src/components/TopNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopNav() {
+  return (
+    <div className="top-nav">
+      
+      Top Nav</div>
+  )
+}
+
+export default TopNav
