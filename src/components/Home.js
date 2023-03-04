c993278d7a322575f3ce242199217ea3b095b0e8
Andrew Cook <andrewjcook93@gmail.com>
2023-03-03 23:25:05 -0600

    new homepage idea

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import HeaderMain from './HeaderMain'
+import Featured from './Featured'
+import Play from './Play'
+
 function Home() {
   return (
-    <div className="home">
-            
-        <div className="home__textbox">
-          <h1 className="heading-primary heading-primary--home">
-              <span className="heading-primary--main">
-                Card Games</span>
-              <span className="heading-primary--sub">
-                by Web Slammers</span>
-          </h1>
-
-          <br /><br />
-          
-          <Link to="/play" className="btn btn--home">
-              Play<span></span></Link>
-              
-
-        </div>
-
-        <div className="home__bg">&nbsp;</div>
-
+    <div>
+      
+      <HeaderMain />
+      <Featured />
+      {/* <Play /> */}
 
     </div>
   )
